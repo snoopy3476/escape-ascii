0dc645b3b39e85f71ed2bc19ad93b5447d6cd1ca
Kim Hwiwon <kim.hwiwon@outlook.com>
2023-06-30 10:58:30 +0900

    refac: change to use stack only for buf and processing

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,21 @@ use std::io::{stdin, stdout, Read, Write};
 
 fn main() {
     let (stdin, mut stdout) = (stdin(), stdout());
-    let mut buf = [0_u8; 4096];
+    let mut inbuf = [0_u8; 1024];
+    let mut outbuf = [0_u8; 4096];
     loop {
-        match stdin.lock().read(&mut buf) {
+        match stdin.lock().read(&mut inbuf) {
             Ok(0) | Err(_) => break,
             Ok(n) => {
                 stdout
-                    .write_all(&buf[..n].escape_ascii().collect::<Vec<u8>>())
+                    .write_all({
+                        let write_bytes = inbuf[..n].escape_ascii().fold(0, |idx, byte| {
+                            outbuf[idx] = byte;
+                            idx + 1
+                        });
+
+                        &outbuf[..write_bytes]
+                    })
                     .ok();
                 stdout.flush().ok();
             }
